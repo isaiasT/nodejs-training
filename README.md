13e97950591a41b809e604c277725cef5237df27
Isaias Tortosa <isaias.tortosa@jobandtalent.com>
2022-02-23 16:45:33 +0100

    Added body-parser

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # nodejs-training
+
+## Build docker image
+
+    make build
+
+## Run docker image
+
+    make run
--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
 const port = 3000
 
+var jsonParser = bodyParser.json()
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.post('/login', urlencodedParser, function (req, res) {
+    res.send('Welcome, ' + req.body.username)
+})
+
+app.post('/api/users', jsonParser, function (req, res) {
+    res.send('Created user: ' + req.body.username)
 })
 
 app.listen(port, () => {
