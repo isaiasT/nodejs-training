f98bb365093be4b90c5811938f58add21ab2c404
Isaias Tortosa <isaias.tortosa@jobandtalent.com>
2022-02-24 16:55:55 +0100

    Added Rollbar

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+require('dotenv').config();
 const express = require('express')
 const bodyParser = require('body-parser')
 
@@ -9,16 +10,26 @@ var jsonParser = bodyParser.json()
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar")
+var rollbar = new Rollbar({
+    accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.post('/login', urlencodedParser, function (req, res) {
     res.send('Welcome, ' + req.body.username)
+    rollbar.log('Welcome, ' + req.body.username)
 })
 
 app.post('/api/users', jsonParser, function (req, res) {
     res.send('Created user: ' + req.body.username)
+    rollbar.log('Created user: ' + req.body.username)
 })
 
 app.listen(port, () => {
