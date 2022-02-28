68e691924eda16bb588a11fb179144a257b6e812
Isaias Tortosa <isaias.tortosa@jobandtalent.com>
2022-02-28 12:53:37 +0100

    Make use of body-parser JSON as default

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,7 @@ import Rollbar from 'rollbar';
 const app = express();
 const port = 3000;
 
-const jsonParser = bodyParser.json();
-
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(bodyParser.json());
 
 const rollbar = new Rollbar({
     accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
@@ -20,12 +18,12 @@ app.get('/', (_req: Request, res: Response) => {
     res.send('Hello World!');
 });
 
-app.post('/login', urlencodedParser, (req: Request, res: Response) => {
+app.post('/login', (req: Request, res: Response) => {
     res.send('Welcome, ' + req.body.username);
     rollbar.log('Welcome, ' + req.body.username);
 });
 
-app.post('/api/users', jsonParser, (req: Request, res: Response) => {
+app.post('/api/users', (req: Request, res: Response) => {
     res.send('Created user: ' + req.body.username);
     rollbar.log('Created user: ' + req.body.username);
 });
